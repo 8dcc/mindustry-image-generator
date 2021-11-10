877d5983125d6535178b34de25ef1f78ce35740d
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2021-11-10 13:28:53 +0000

    v2.12

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
-// Adds some elements as variables: file upload button, source image, and code output box
+// For comments check the original repo
 let imgElement = document.getElementById('imageSrc');
 let inputElement = document.getElementById('fileInput');
-let codeoutput = document.getElementById('codeoutput');
+// let codeoutput0 = document.getElementById('codeoutput0');
+// let codeoutput1 = document.getElementById('codeoutput1');
+// let codeoutput2 = document.getElementById('codeoutput2');
+// let codeoutput3 = document.getElementById('codeoutput3');
 let schematicTextArea = document.getElementById('schematicTextArea');
 inputElement.addEventListener('change', (e) => {
     imgElement.src = URL.createObjectURL(e.target.files[0]);
 }, false);
 
-schematicTextArea.value="bXNjaAF4nJ2Pu07DMBSGTy5qpXJb080TLCQZurFWYuAiMfQFXNtNLBqf6NhRyYYYWJAYEGPfgNdgKw/AzMwbMBQnwAsgL/Z/fvn7DkQQhxAbXilILrDQgklt6yVv2QKJ6YoXysKOVFaQrp1GA9NTP2isYivtSlY6V9uTPLeNxLnixmaFj5t5pjGvtJGNddSm/T9poYwi7pDyGPaWHSz9hcGwUtb6DhxUWhCmNaHwCRIMrOeIEgBigKA7gb9HcQAv/2OPRjPs/V2p7bFfsUZy/qGYQKnYgrBiLTbExFLXc+QkmTYO+8ZlJ8eu/uQyNiuV6Yriup//uDLflsRXfdTjsyj05tG+Nz+6WQt5C4eDx+e3j7v169l0cL/dJu8Pm8n502R3nIwTaD+HX0EEIUQjCL8BOt6J8A==";
+schematicTextArea.value = "bXNjaAF4nJ2Pu07DMBSGTy5qpXJb080TLCQZurFWYuAiMfQFXNtNLBqf6NhRyYYYWJAYEGPfgNdgKw/AzMwbMBQnwAsgL/Z/fvn7DkQQhxAbXilILrDQgklt6yVv2QKJ6YoXysKOVFaQrp1GA9NTP2isYivtSlY6V9uTPLeNxLnixmaFj5t5pjGvtJGNddSm/T9poYwi7pDyGPaWHSz9hcGwUtb6DhxUWhCmNaHwCRIMrOeIEgBigKA7gb9HcQAv/2OPRjPs/V2p7bFfsUZy/qGYQKnYgrBiLTbExFLXc+QkmTYO+8ZlJ8eu/uQyNiuV6Yriup//uDLflsRXfdTjsyj05tG+Nz+6WQt5C4eDx+e3j7v169l0cL/dJu8Pm8n502R3nIwTaD+HX0EEIUQjCL8BOt6J8A==";
 
-// This code adds functionality to the collapsible menus. 
-// Stolen from https://www.w3schools.com/howto/howto_js_collapsible.asp
 var collapsibleMenus = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < collapsibleMenus.length; i++) {
-  collapsibleMenus[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-} 
+    collapsibleMenus[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
 
-// Generates and displays the code when an image is uploaded
 imgElement.onload = function() {
-  let newSize = new cv.Size(40, 40); // The resolution of the image for the display
+    let newSize = new cv.Size(44, 44);
 
-  let uploadedImage = cv.imread(imgElement); // Loads the uploaded image as an OpenCV mat
-  let resizedImage = new cv.Mat();
+    let uploadedImage = cv.imread(imgElement);
+    let resizedImage = new cv.Mat();
 
-  cv.resize(uploadedImage, resizedImage, newSize, cv.INTER_NEAREST); // Resizes the image
-  cv.imshow('resizedImageCanvas', resizedImage);
+    cv.resize(uploadedImage, resizedImage, newSize, cv.INTER_NEAREST);
+    cv.imshow('resizedImageCanvas', resizedImage);
 
-  codeoutput.value = imageToCode(resizedImage, 160);
+    for (let i = 0; i <= 3; i++) {
+        document.getElementById('codeoutput' + i).value = imageToCode(resizedImage, i, 176);
+    }
 
-  uploadedImage.delete();
-  resizedImage.delete();
+    uploadedImage.delete();
+    resizedImage.delete();
 };
+
+// Selects and copies the code in the code box
+function copyCodeOutput(i) {
+  document.getElementById('codeoutput' + i).select();
+  document.getElementById('codeoutput' + i).setSelectionRange(0, 999999);
+  document.execCommand("copy")
+}
 
 // Selects and copies the code in the code box
 function copySchematic() {
@@ -49,30 +58,39 @@ function copySchematic() {
   schematicTextArea.execCommand("copy")
 }
 
-// Converts an image to code
-function imageToCode(image, displaySize) {
-  let imageAsCode = "";
-  let pixel = 0;
-  let pixelSize = displaySize / image.rows;
+function imageToCode(image, number, displaySize) {
+    let imageAsCode = "";
+    let pixel = 0;
+    let pixelSize = displaySize / image.rows;
 
-  imageAsCode += "sensor draw switch1 @enabled\n" +
-                  "jump 3 equal draw true\n" +
-                  "end\n"+
-                  "draw clear 0 0 0 0 0 0 \n"
-
-  for(let x = 0; x < image.cols; x++) {
-    for(let y = 0; y < image.rows; y++) {
-      pixel = image.ucharPtr(y, x);
-      imageAsCode += `draw color ${pixel[0]} ${pixel[1]} ${pixel[2]} ${pixel[3]} 0 0\n`;
-                                                      /* the displaySize - (y + 1) * pixelSize is necessary because 
-                                                      the top corner in OpenCV is (0, 0) 
-                                                      but in Mindustry it is (80, 0)  */
-      imageAsCode += `draw rect ${x * pixelSize} ${displaySize - ((y + 1) * pixelSize)} ${pixelSize} ${pixelSize} 0 0\n`;
+    if (number === 0) {
+        imageAsCode += "sensor draw switch1 @enabled\n" +
+            "jump 3 equal draw true\n" +
+            "end\n" +
+            "draw clear 0 0 0 0 0 0 \n" +
+            "write 1 cell1 0\n"
+    } else {
+        imageAsCode += "sensor draw switch1 @enabled\n" +
+            "read p cell1 0\n" +
+            "jump 4 equal draw true\n" +
+            "end\n" +
+            "jump 6 equal p 1\n" +
+            "end\n"
     }
-    imageAsCode += "drawflush display1\n"
-  }
 
-  imageAsCode += "control enabled switch1 false 0 0 0\n";
+    for (let x = number*11; x < number*11+11; x++) {
+        for (let y = 0; y < image.rows; y++) {
+            pixel = image.ucharPtr(y, x);
+            imageAsCode += `draw color ${pixel[0]} ${pixel[1]} ${pixel[2]} ${pixel[3]} 0 0\n`;
+            imageAsCode += `draw rect ${x * pixelSize} ${displaySize - ((y + 1) * pixelSize)} ${pixelSize} ${pixelSize} 0 0\n`;
+        }
+        imageAsCode += "drawflush display1\n"
+    }
 
-  return imageAsCode;
+    if (number === 0) {
+        imageAsCode += "control enabled switch1 false 0 0 0\n"+
+                        "write 0 cell1 0"
+    }
+
+    return imageAsCode;
 }
